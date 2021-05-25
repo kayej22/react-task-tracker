d3877d407ec0c13bf36064c99a9df5bdad4aee48
Joel Kaye <kayej22@hotmail.com>
2021-05-25 10:26:13 -0600

    setting up button component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import PropTypes from 'prop-types';
+import Button from './Button'
 
-
+// setting up header
 const Header = ({ title }) => {
+
+  const onClick = () => {
+    console.log('click')
+  }
+
   return (
-    <header>
+    <header className='header'>
       <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
     </header>
   )
 }
-
+// setting default title
 Header.defaultProps = {
   title: 'Task Tracker'
 }
 
+// requiring string for title
 Header.propTypes = {
   title: PropTypes.string.isRequired,
 }
 
-const headingStyle = {
-  color: 'red', backgroundColor: 'black'
-}
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// }
 
 export default Header
