5446a67a58b6e82935f68db047eae4716033d92b
Joel Kaye <kayej22@hotmail.com>
2021-05-27 09:57:40 -0600

    working on tasks.js

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,8 @@
-const tasks = [
-  {
-    id:'1',
-    text: 'Doctors Appointment',
-    day:'Feb 5th at 2:30pm',
-    reminder: true,
-  },
-  {
-    id:'2',
-    text: 'Meeting at School',
-    day:'Feb 6th at 1:30pm',
-    reminder: true,
-  },
-  {
-    id:'3',
-    text: 'Food Shopping',
-    day:'Feb 5th at 2:30pm',
-    reminder: false,
-  }
-]
-
-export const Tasks = () => {
+const Tasks = ({ tasks }) => {
   return (
     <>
       {tasks.map((task) => (
-        <h3>{task.text}</h3>
+        <h3 key={tasks.id}>{task.text}</h3>
       ))}
     </>
   )
