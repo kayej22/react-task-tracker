c36eac9851f31f7db1092960853740d637667084
Joel Kaye <kayej22@hotmail.com>
2021-05-28 09:52:16 -0600

    working on task toggle

--- src/App.js
+++ src/App.js
@@ -29,10 +29,17 @@ const App = () => {
     setTasks(tasks.filter((task) => task.id !== id ))
   }
 
+  // toggle reminder
+  const toggleReminder =(id)=> {
+    console.log(id);
+  }
+
+
   return (
     <div className="container">
      <Header />
-     <Tasks tasks={tasks} onDelete= {deleteTask} />
+    {tasks.length > 0 ?
+      <Tasks tasks={tasks} onDelete= {deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
 }
