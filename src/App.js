d19fb7de030e14c987c8de706c8e71c2e96c5c02
Joel Kaye <kayej22@hotmail.com>
2021-05-31 08:34:47 -0600

    show add task toggle

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
 
 const App = () => {
+  const [showAddTask, setShowAddTask] = useState(false)
   const [tasks, setTasks] = useState([
     {
       id:'1',
@@ -29,6 +30,7 @@ const App = () => {
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
     const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
   }
 
   // Delete task
@@ -46,8 +48,8 @@ const App = () => {
 
   return (
     <div className="container">
-     <Header />
-     <AddTask onAdd={addTask}/>
+     <Header onAdd={() => setShowAddTask(!showAddTask)}/>
+     {showAddTask && <AddTask onAdd={addTask}/>}
     {tasks.length > 0 ?
       <Tasks tasks={tasks} onDelete= {deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
