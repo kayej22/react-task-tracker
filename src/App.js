3d668e4ae8723b07b9d70ef804628f5a7360e495
Joel Kaye <kayej22@hotmail.com>
2021-05-31 08:51:30 -0600

    button toggle

--- src/App.js
+++ src/App.js
@@ -48,10 +48,15 @@ const App = () => {
 
   return (
     <div className="container">
-     <Header onAdd={() => setShowAddTask(!showAddTask)}/>
-     {showAddTask && <AddTask onAdd={addTask}/>}
+     <Header
+      onAdd={() => setShowAddTask(!showAddTask)}
+      showAdd={showAddTask}/>
+     {showAddTask && <AddTask onAdd={addTask} />}
     {tasks.length > 0 ?
-      <Tasks tasks={tasks} onDelete= {deleteTask} onToggle={toggleReminder} /> : 'No Tasks To Show'}
+      <Tasks
+        tasks={tasks}
+        onDelete= {deleteTask}
+        onToggle={toggleReminder} /> : 'No Tasks To Show'}
     </div>
   );
 }
